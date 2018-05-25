8cac62ef88ff862a9dde9ecc7897e4f58832c328
joshi-git-hub <g80joshi@gmail.com>
2018-05-25 08:16:22 +0530

    this is my program for taking input during program

---
+++ myread.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this program is about how to take input during program.
+# we will user read variable for getting input during program.
+echo "wellcome all guy's, very very Good morning."
+echo "What is your name Mis."
+read i
+echo "Ok, Thanks $i. for telling your name. $i is nice name. thankyou."
+
